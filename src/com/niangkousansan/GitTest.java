6c207929086bcdde5251fab9c2f515a2bb9e65c1
mkx <392287629@qq.com>
2025-01-29 09:28:55 +0800

    first commit

---
+++ src/com/niangkousansan/GitTest.java
@@ -0,0 +1,11 @@
+package com.niangkousansan;
+
+/**
+ * @author 马可新
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
